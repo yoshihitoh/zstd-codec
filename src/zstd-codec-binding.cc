5b24f0279beb6ae91a75d22795d54a5dac273fcf
Yoshihito Hirakawa <hammer.and.heart.daphne@gmail.com>
2017-12-29 14:59:30 +0900

    Extract streaming-api from ZstdCodec to Compress/Decompress Stream class, and disable streaming tests (temporary)

--- src/zstd-codec-binding.cc
+++ src/zstd-codec-binding.cc
@@ -1,8 +1,11 @@
+#include <algorithm>
 #include <climits>
 #include <cstdint>
 #include <cstdio>
 #include <cstddef>
 #include <cstdio>
+#include <iterator>
+#include <memory>
 #include <vector>
 #include <emscripten/bind.h>
 
@@ -31,7 +34,7 @@ void dummy()
 
 // NOTE: ref: https://github.com/kripken/emscripten/issues/5519
 template<typename T>
-static size_t copy_to_vector(Vec<T> &dest, const val &src)
+static size_t copy_to_vector(Vec<T>& dest, const val& src)
 {
     const auto length = src["length"].as<unsigned int>();
 
@@ -46,74 +49,21 @@ static size_t copy_to_vector(Vec<T> &dest, const val &src)
 
 
 template <typename T>
-static Vec<T> from_js_typed_array(const val& src)
+static void resize_to_fit(Vec<T>& dest, const val& src)
 {
-    Vec<T> dest;
     dest.resize(src["length"].as<size_t>());
-    auto length = copy_to_vector(dest, src);
-
-    return std::move(dest);
 }
 
 
 template <typename T>
-class Resource
+static Vec<T> from_js_typed_array(const val& src)
 {
-public:
-    using Closer = std::function<void(T*)>;
+    Vec<T> dest;
+    resize_to_fit(dest, src);
+    auto length = copy_to_vector(dest, src);
 
-    Resource(T* resource, Closer closer)
-        : resource_(resource)
-        , closer_(closer)
-    {
-    }
-
-    ~Resource()
-    {
-        if (resource_ != nullptr) {
-            closer_(resource_);
-        }
-    }
-
-protected:
-    T* resource() const {
-        return resource_;
-    }
-
-private:
-    T* resource_;
-    Closer closer_;
-};
-
-
-class ZstdCStreamResource : public Resource<ZSTD_CStream>
-{
-public:
-    ZstdCStreamResource()
-        : Resource(ZSTD_createCStream(), ZSTD_freeCStream)
-    {
-    }
-
-    ZSTD_CStream* stream() const
-    {
-        return resource();
-    }
-};
-
-
-class ZstdDStreamResource : public Resource<ZSTD_DStream>
-{
-public:
-    ZstdDStreamResource()
-        : Resource(ZSTD_createDStream(), ZSTD_freeDStream)
-    {
-    }
-
-    ZSTD_DStream* stream() const
-    {
-        return resource();
-    }
-};
+    return std::move(dest);
+}
 
 
 static size_t read_from_js_callback(val callback, Vec<u8>& buffer, size_t read_size)
@@ -132,7 +82,7 @@ static void write_to_js_callback(val callback, const Vec<u8>& buffer, size_t wri
 }
 
 
-// ---- bindings --------------------------------------------------------------
+// ---- basic apis ------------------------------------------------------------
 
 
 bool is_error(int zstd_rc)
@@ -187,39 +137,6 @@ int compress(uintptr_t dest, size_t dest_size, val src, int compression_level)
 }
 
 
-bool compress_stream(val read_callback, val write_callback, int compression_level)
-{
-    ZstdCStreamResource cstream;
-    if (cstream.stream() == nullptr) return false;
-
-    Vec<u8> in_bytes(ZSTD_CStreamInSize());
-    Vec<u8> out_bytes(ZSTD_CStreamOutSize());
-
-    const auto init_rc = ZSTD_initCStream(cstream.stream(), compression_level);
-    if (ZSTD_isError(init_rc)) return false;
-
-    auto read_size = in_bytes.size();
-    size_t read_done;
-    while ((read_done = read_from_js_callback(read_callback, in_bytes, read_size))) {
-        ZSTD_inBuffer in_buff { &in_bytes[0], read_done, 0 };
-        while (in_buff.pos < in_buff.size) {
-            ZSTD_outBuffer out_buff { &out_bytes[0], out_bytes.size(), 0 };
-            read_size = ZSTD_compressStream(cstream.stream(), &out_buff, &in_buff);
-            if (ZSTD_isError(read_size)) return false;
-
-            write_to_js_callback(write_callback, out_bytes, out_buff.pos);
-        }
-    }
-
-    ZSTD_outBuffer out_buff { &out_bytes[0], out_bytes.size(), 0 };
-    const auto remaining = ZSTD_endStream(cstream.stream(), &out_buff);
-    if (remaining > 0u) return false;
-    write_to_js_callback(write_callback, out_bytes, out_buff.pos);
-
-    return true;
-}
-
-
 int decompress(uintptr_t dest, size_t dest_size, val src)
 {
     auto src_bytes = from_js_typed_array<u8>(src);
@@ -236,33 +153,281 @@ int decompress(uintptr_t dest, size_t dest_size, val src)
 }
 
 
-bool decompress_stream(val read_callback, val write_callback)
+// ---- streaming apis (declarations) -----------------------------------------
+
+
+class ZstdCompressStream
 {
-    ZstdDStreamResource dstream;
-    if (dstream.stream() == nullptr) return false;
+public:
+    ZstdCompressStream();
+    ~ZstdCompressStream();
 
-    Vec<u8> in_bytes(ZSTD_DStreamInSize());
-    Vec<u8> out_bytes(ZSTD_DStreamOutSize());
+    bool Begin(int compression_level);
+    bool Transform(val chunk, val write_callback);
+    bool Flush(val write_callback);
+    bool End(val write_callback);
 
-    const auto init_rc = ZSTD_initDStream(dstream.stream());
+private:
+    using CStreamPtr = std::unique_ptr<ZSTD_CStream, decltype(&ZSTD_freeCStream)>;
+
+    bool HasStream() const;
+    bool Compress(val write_callback);
+
+    CStreamPtr  stream_;
+    size_t      next_read_size_;
+    Vec<u8>     src_bytes_;
+    Vec<u8>     dest_bytes_;
+    Vec<u8>     copy_bytes_;
+};
+
+
+class ZstdDecompressStream
+{
+public:
+    ZstdDecompressStream();
+    ~ZstdDecompressStream();
+
+    bool Begin();
+    bool Transform(val chunk, val write_callback);
+    bool Flush(val write_callback);
+    bool End(val write_callback);
+
+private:
+    using DStreamPtr = std::unique_ptr<ZSTD_DStream, decltype(&ZSTD_freeDStream)>;
+
+    bool HasStream() const;
+    bool Decompress(val write_callback);
+
+    DStreamPtr  stream_;
+    size_t      next_read_size_;
+    Vec<u8>     src_bytes_;
+    Vec<u8>     dest_bytes_;
+    Vec<u8>     copy_bytes_;
+};
+
+
+// ---- streaming apis (implementations) --------------------------------------
+
+//
+// ZstdCompressStream
+//
+///////////////////////////////////////////////////////////////////////////////
+
+
+ZstdCompressStream::ZstdCompressStream()
+    : stream_(nullptr, ZSTD_freeCStream)
+    , next_read_size_()
+    , src_bytes_()
+    , dest_bytes_()
+    , copy_bytes_()
+{
+}
+
+
+ZstdCompressStream::~ZstdCompressStream()
+{
+}
+
+
+bool ZstdCompressStream::Begin(int compression_level)
+{
+    if (HasStream()) return true;
+
+    CStreamPtr stream(ZSTD_createCStream(), ZSTD_freeCStream);
+    if (stream == nullptr) return false;
+
+    const auto init_rc = ZSTD_initCStream(stream.get(), compression_level);
     if (ZSTD_isError(init_rc)) return false;
 
-    auto read_size = init_rc;
-    auto read_done = init_rc;
-    while ((read_done = read_from_js_callback(read_callback, in_bytes, read_size))) {
-        ZSTD_inBuffer in_buff { &in_bytes[0], read_done, 0 };
-        while (in_buff.pos < in_buff.size) {
-            ZSTD_outBuffer out_buff { &out_bytes[0], out_bytes.size(), 0 };
-            read_size = ZSTD_decompressStream(dstream.stream(), &out_buff, &in_buff);
-            if (ZSTD_isError(read_size)) return false;
-
-            write_to_js_callback(write_callback, out_bytes, out_buff.pos);
-        }
-    }
+    stream_ = std::move(stream);
+    src_bytes_.reserve(ZSTD_CStreamInSize());
+    dest_bytes_.resize(ZSTD_CStreamOutSize());  // resize
+    copy_bytes_.reserve(src_bytes_.size());
+    next_read_size_ = src_bytes_.size();
 
     return true;
 }
 
+
+bool ZstdCompressStream::Transform(val chunk, val write_callback)
+{
+    if (!HasStream()) return false;
+
+    // copy from TypedArray(js) to Vector(c++)
+    resize_to_fit(copy_bytes_, chunk);
+    copy_to_vector(copy_bytes_, chunk);
+
+    // append src bytes
+    std::copy(std::begin(copy_bytes_),
+              std::end(copy_bytes_), std::back_inserter(src_bytes_));
+
+    // not enough
+    if (src_bytes_.size() + copy_bytes_.size() < next_read_size_) {
+        return true;
+    }
+
+    // enough
+    return Compress(write_callback);
+}
+
+
+bool ZstdCompressStream::Flush(val write_callback)
+{
+    return Compress(write_callback);
+}
+
+
+bool ZstdCompressStream::End(val write_callback)
+{
+    if (!HasStream()) return true;
+
+    auto success = true;
+    if (!src_bytes_.empty()) {
+        success = Compress(write_callback);
+    }
+
+    if (success) {
+        ZSTD_outBuffer output { &dest_bytes_[0], dest_bytes_.size(), 0 };
+        const auto remaining = ZSTD_endStream(stream_.get(), &output);
+        if (remaining > 0u) return false;
+        write_to_js_callback(write_callback, dest_bytes_, output.pos);
+    }
+
+    stream_.reset();
+    return success;
+}
+
+
+bool ZstdCompressStream::HasStream() const
+{
+    return stream_ != nullptr;
+}
+
+
+bool ZstdCompressStream::Compress(val write_callback)
+{
+    if (src_bytes_.empty()) return true;
+
+    ZSTD_inBuffer input { &src_bytes_[0], src_bytes_.size(), 0 };
+    while (input.pos < input.size) {
+        ZSTD_outBuffer output { &dest_bytes_[0], dest_bytes_.size(), 0};
+        next_read_size_ = ZSTD_compressStream(stream_.get(), &output, &input);
+        if (ZSTD_isError(next_read_size_)) return false;
+
+        write_to_js_callback(write_callback, dest_bytes_, output.pos);
+    }
+
+    src_bytes_.clear();
+    return true;
+}
+
+
+//
+// ZstdDecompressStream
+//
+///////////////////////////////////////////////////////////////////////////////
+
+ZstdDecompressStream::ZstdDecompressStream()
+    : stream_(nullptr, ZSTD_freeDStream)
+    , next_read_size_()
+    , src_bytes_()
+    , dest_bytes_()
+{
+}
+
+
+ZstdDecompressStream::~ZstdDecompressStream()
+{
+}
+
+
+bool ZstdDecompressStream::Begin()
+{
+    if (HasStream()) return true;
+
+    DStreamPtr stream(ZSTD_createDStream(), ZSTD_freeDStream);
+    if (stream == nullptr) return false;
+
+    const auto init_rc = ZSTD_initDStream(stream.get());
+    if (ZSTD_isError(init_rc)) return false;
+
+    stream_ = std::move(stream);
+    src_bytes_.reserve(ZSTD_DStreamInSize());
+    dest_bytes_.resize(ZSTD_DStreamOutSize());  // resize
+    copy_bytes_.reserve(src_bytes_.size());
+    next_read_size_ = init_rc;
+
+    return true;
+}
+
+
+bool ZstdDecompressStream::Transform(val chunk, val write_callback)
+{
+    if (!HasStream()) return false;
+
+    // copy from TypedArray(js) to Vector(c++)
+    resize_to_fit(copy_bytes_, chunk);
+    copy_to_vector(copy_bytes_, chunk);
+
+    // append src bytes
+    std::copy(std::begin(copy_bytes_),
+              std::end(copy_bytes_), std::back_inserter(src_bytes_));
+
+    // not enough
+    if (src_bytes_.size() + copy_bytes_.size() < next_read_size_) {
+        return true;
+    }
+
+    // enough
+    return Decompress(write_callback);
+}
+
+
+bool ZstdDecompressStream::Flush(val write_callback)
+{
+    return Decompress(write_callback);
+}
+
+
+bool ZstdDecompressStream::End(val write_callback)
+{
+    if (!HasStream()) return true;
+
+    auto success = true;
+    if (!src_bytes_.empty()) {
+        success = Decompress(write_callback);
+    }
+
+    stream_.reset();
+    return success;
+}
+
+
+bool ZstdDecompressStream::HasStream() const
+{
+    return stream_ != nullptr;
+}
+
+
+bool ZstdDecompressStream::Decompress(val write_callback)
+{
+    if (src_bytes_.empty()) return true;
+
+    ZSTD_inBuffer input { &src_bytes_[0], src_bytes_.size(), 0 };
+    while (input.pos < input.size) {
+        ZSTD_outBuffer output { &dest_bytes_[0], dest_bytes_.size(), 0};
+        next_read_size_ = ZSTD_decompressStream(stream_.get(), &output, &input);
+        if (ZSTD_isError(next_read_size_)) return false;
+
+        write_to_js_callback(write_callback, dest_bytes_, output.pos);
+    }
+
+    src_bytes_.clear();
+    return true;
+}
+
+
+// ---- bindings --------------------------------------------------------------
 
 EMSCRIPTEN_BINDINGS(zstd) {
     constant("ERR_UNKNOWN", ERR_UNKNOWN);
@@ -272,9 +437,23 @@ EMSCRIPTEN_BINDINGS(zstd) {
     function("compress_bound", &compress_bound);
     function("content_size", &content_size, allow_raw_pointers());
     function("compress", &compress, allow_raw_pointers());
-    function("compress_stream", &compress_stream);
     function("decompress", &decompress, allow_raw_pointers());
-    function("decompress_stream", &decompress_stream);
     function("dummy", &dummy);
+
+    class_<ZstdCompressStream>("ZstdCompressStream")
+        .constructor<>()
+        .function("Begin", &ZstdCompressStream::Begin)
+        .function("Transform", &ZstdCompressStream::Transform)
+        .function("Flush", &ZstdCompressStream::Flush)
+        .function("End", &ZstdCompressStream::End)
+        ;
+
+    class_<ZstdDecompressStream>("ZstdDecompressStream")
+        .constructor<>()
+        .function("Begin", &ZstdDecompressStream::Begin)
+        .function("Transform", &ZstdDecompressStream::Transform)
+        .function("Flush", &ZstdDecompressStream::Flush)
+        .function("End", &ZstdDecompressStream::End)
+        ;
 }
 
