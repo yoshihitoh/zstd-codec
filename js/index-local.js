24a17345ee41ea870990061ffaea63a3ccab8584
Yoshihito Hirakawa <hammer.and.heart.daphne@gmail.com>
2018-01-23 00:04:12 +0900

    Update browser example, use dictionary instance

--- js/index-local.js
+++ js/index-local.js
@@ -1,2 +1,5 @@
 window.ZstdCodec = require('./lib/zstd-codec.js').ZstdCodec;
 window.ZstdStream = require('./lib/zstd-stream.js');
+
+window.ZstdCompressionDict = require('./lib/zstd-dict.js').ZstdCompressionDict;
+window.ZstdDecompressionDict = require('./lib/zstd-dict.js').ZstdDecompressionDict;
