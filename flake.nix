d257ed039f4572a289422fe1d0888d89d54cf132
yoshihitoh <yoshihito.arih@gmail.com>
2023-02-08 01:12:25 +0900

    wip: add nix dependencies

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "A flake of zstd-codec project";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+  };
+}
